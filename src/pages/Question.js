bcc9eb85fb30cf5fb5628c29eec51d14784dbc6e
dlghdus9949 <hoyeon9949@naver.com>
2023-05-08 16:19:10 +0900

    add home page

---
+++ src/pages/Question.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question = () => {
+    return(
+        <div>
+            홈화면
+        </div>
+    );
+}
+
+export default Question;
---
+++ src/pages/Result.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Result = () => {
+    return(
+        <div>
+            홈화면
+        </div>
+    );
+}
+
+export default Result;
