e1a0eddda17c4930b331ec4c1ab4162d9f1a5f1e
dlghdus9949 <hoyeon9949@naver.com>
2023-05-22 13:00:17 +0900

    add result page

---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,131 @@
+import RussianBlue from '../cat/RussianBlue.jpeg';
+import TurkishAngora from '../cat/TurkishAngora.jpeg';
+import Bengal from '../cat/Bengal.jpeg';
+import Munchkin from '../cat/Munchkin.jpeg';
+import ScottishFold from '../cat/ScottishFold.png';
+import Siamese from '../cat/Siamese.jpeg';
+import AmericanShorthair from '../cat/AmericanShorthair.png';
+import Persian from '../cat/Persian.jpg';
+import BritishShorthair from '../cat/BritishShorthair.jpeg';
+import Sphinx from '../cat/Sphinx.png';
+import Exotic from '../cat/Exotic.jpeg';
+import NorwegianForest from '../cat/NorwegianForest.png';
+import KoreanShorthair from '../cat/KoreanShorthair.jpeg';
+import Abyssinian from '../cat/Abyssinian.png';
+import MaineCoon from '../cat/MaineCoon.jpeg';
+import Ragdoll from '../cat/Ragdoll.jpeg';
+
+export const ResultData = [
+    {
+        id:1,
+        name: '러시안 블루',
+        best: 'ISFJ',
+        desc: '조용하며 애정이 많아요! 겸손하다는 말을 자주 들으며 공감 능력이 굉장히 뛰어나요',
+        image: `${RussianBlue}`,
+    },
+    {
+        id:2,
+        name: '터키시 앙고라',
+        best: 'ENFJ',
+        desc: '사람들을 이끄는 것을 좋아해요! 친구들과 쉽게 어울리며 센스가 있고 눈치가 빨라요',
+        image: `${TurkishAngora}`,
+    },
+    {
+        id:3,
+        name: '뱅갈',
+        best: 'ESTP',
+        desc: '리더십이 뛰어나요! 공감 능력이 조금 부족하지만 화나는 일, 속상한 일 금방 잊어버려요',
+        image: `${Bengal}`,
+    },
+    {
+        id:4,
+        name: '먼치킨',
+        best: 'ENTJ',
+        desc: '피해 주거나 피해 받는 것을 싫어해요. 현실적으로 생각하는 경향이 있으며 논리적으로 이야기 해요',
+        image: `${Munchkin}`,
+    },
+    {
+        id:5,
+        name: '스코티시 폴드',
+        best: 'ISFP',
+        desc: '공감 능력이 뛰어나요! 무기력한 날들이 많지만 열정이 생겼을 땐 그 누구보다 열심히 해요',
+        image: `${ScottishFold}`,
+    },
+    {
+        id:6,
+        name: '샴',
+        best: 'INFP',
+        desc: '게으른 완벽주의자예요! 좋아하는 것은 열정 가득 한 우물 파지만, 열정이 쉽게 식는 타입이에요',
+        image: `${Siamese}`,
+    },
+    {
+        id:7,
+        name: '아메리칸 숏헤어',
+        best: 'ESFP',
+        desc: '평화주의자예요! 논쟁이 있길 원치 않아고 사교성이 뛰어나서 처음 본 사람들과도 잘 친해져요!',
+        image: `${AmericanShorthair}`
+    },
+    {
+        id:8,
+        name: '페르시안',
+        best: 'INFJ',
+        desc: '굉장한 집순이/집돌이 타입이에요! 생각이 유독 많고 낯가림이 심해요. 해야 할 일은 미리미리 해치우며 계획적으로 행동해요.',
+        image: `${Persian}`,
+    },
+    {
+        id:9,
+        name: '브리티시 숏헤어',
+        best: 'ISTP',
+        desc: '혼자의 생활이 편한 귀차니즘, 마이웨이 스타일이에요! ',
+        image: `${BritishShorthair}`
+    },
+    {
+        id:10,
+        name: '스핑크스',
+        best: 'ENTP',
+        desc: '독립심이 강하며 자존심이 강해요! 지나간 일에 연연하지 않으며 자기합리화를 자주 하곤 해요.',
+        image: `${Sphinx}`,
+    },
+    {
+        id:11,
+        name: '엑죠틱',
+        best: 'ESTJ',
+        desc: '빠릿빠릿한 성격이예요! 일 처리를 잘 하며 논리 정연하게 이야기를 잘 해요.',
+        image: `${Exotic}`,
+    },
+    {
+        id:12,
+        name: '노르웨이 숲',
+        best: 'INTP',
+        desc: '분석하고 추리하는 것을 좋아해요! 논리정연한 것을 좋아하며 내용에 뒷받침 되는 근거가 있길 원해요.',
+        image: `${NorwegianForest}`,
+    },
+    {
+        id:13,
+        name: '코리안 숏헤어',
+        best: 'ENFP',
+        desc: '나서서 하길 좋아하진 않지만 아무도 나서지 않을 때 나서는 타입이며, 낯을 가리지만 친해지면 수다쟁이에요!',
+        image: `${KoreanShorthair}`,
+    },
+    {
+        id:14,
+        name: '아비시니안',
+        best: 'ESFJ',
+        desc: '철저하게 계획하고 진행하려 하며, 계획대로 흘러가길 원해요. 고민 상담을 잘 해주며, 인간 관계에 유독 신경을 많이 써요.',
+        image: `${Abyssinian}`,
+    },
+    {
+        id:15,
+        name: '메인쿤',
+        best: 'INTJ',
+        desc: '시간별, 순서별 정리하고 계획하는 것을 좋아하며, 완벽하게 해내려고 하는 경향이 있어요.',
+        image: `${MaineCoon}`,
+    },
+    {
+        id:16,
+        name: '렉돌',
+        best: 'ISTJ',
+        desc: '계획적이고 논리적이에요. 협동하기보다 혼자 해결하는 것이 편하고, 간섭 받기 싫어해요. 사람들에게 관심이 많은 편은 아니에요',
+        image: `${Ragdoll}`,
+    },
+]
